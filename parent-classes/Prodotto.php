603af6139147c796baecf1bfd019146815d85c86
simonescoca <simone.scoca@icloud.com>
2023-07-14 18:06:47 +0200

    added main class

--- parent-classes/Prodotto.php
+++ parent-classes/Prodotto.php
@@ -6,14 +6,16 @@
         public $prezzo_euro;
         public $categoria;
         public $rating;
+        public $image;
 
-        function __construct (int $id, string $nome, string $descrizione, float $prezzo_euro, string $categoria, float $rating) {
+        function __construct (int $id, string $nome, string $descrizione, float $prezzo_euro, string $categoria, float $rating, string $image) {
             $this -> id = $id;
             $this -> nome = $nome;
             $this -> descrizione = $descrizione;
             $this -> prezzo_euro = $prezzo_euro;
             $this -> categoria = $categoria;
             $this -> rating = $rating;
+            $this -> image = $image;
         }
 
         // public function  () {
