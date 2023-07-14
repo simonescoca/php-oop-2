1ed9accd6795a92230c88facebe29cdc3f210045
simonescoca <simone.scoca@icloud.com>
2023-07-14 18:06:03 +0200

    added "istanze" file to populate item objects

--- database/istanze.php
+++ database/istanze.php
@@ -6,13 +6,10 @@
 ?>
 
 <?php
-    $medicinale = new Medicinale ("Principio attivissimo", "Una pasticca al secondo", 1, "Medicina per la gattina", "La tua gattina torna nuova", 49.99, "Gatti", 4.5);
-    $cibo = new Cibo ("Snack", "Media", 2, "Snack delizioso", "Il tuo cane si leccherà i baffi che non ha", 2.99, "Cani", 4.0);
-    // $cuccia = new Cuccia ("Tessuto", "Grande", 3, "Cuccia cucciosa", "Provocatrice di pisolini infiniti", 79.39, "Cani", 4.5);
-    $gioco = new Gioco ("Pallina", "Rosso", 4, "Speedball", "Farà impazzire il tuo gatto questa pallina super veloce", 14.50, "Gatti", 5.0);
+    $medicinale = new Medicinale ("Principio attivissimo", "Una pasticca al secondo", 1, "Medicina per la gattina", "La tua gattina torna nuova", 49.99, "Gatti", 4.5, "https://www.helpfarma.it/23019-medium_default/drontal-2cpr-gatto.jpg");
+    $cibo = new Cibo ("Snack", "Media", 2, "Snack delizioso", "Il tuo cane si leccherà i baffi che non ha", 2.99, "Cani", 4.0, "https://qz-petshop.com/8734-large_default/pedigree-snack-per-cani-rodeo-manzo-7-sticks.jpg");
+    $cuccia = new Cuccia ("Tessuto", "Grande", 3, "Cuccia cucciosa", "Provocatrice di pisolini infiniti", 79.39, "Cani", 4.5, "https://m.media-amazon.com/images/I/71-tsDiw8iL._AC_UF894,1000_QL80_.jpg");
+    $gioco = new Gioco ("Pallina", "Rosso", 4, "Speedball", "Farà impazzire il tuo gatto questa pallina super veloce", 14.50, "Gatti", 5.0, "https://m.media-amazon.com/images/I/71yg2KYeq6L._AC_UF894,1000_QL80_.jpg");
 
-    var_dump ($medicinale);
-    var_dump ($cibo);
-    var_dump ($cuccia);
-    var_dump ($gioco);
+    $prodotti = [$medicinale, $cibo, $cuccia, $gioco];
 ?>
